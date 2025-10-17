60fba6e197934490a912ad935acd3985b5318fcb
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-10-17 15:25:07 -0300

    Utilizando o Insomnia

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from "node:http"
 
 const server = http.createServer((request, response) => {
-    return response.end("Teste4")//uma resposta é obrigatória, se não fica procurando eternamente.
+    return response.end("Minha primeira API")//uma resposta é obrigatória, se não fica procurando eternamente.
 })
 
 server.listen(3333);
