00daca941b5ab097af1081ddbf8ff4ab1c8dd179
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-10-18 13:20:27 -0300

    URL da requisição

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
-import http from "node:http"
+import http from "node:http";
 
 const server = http.createServer((request, response) => {
-    const { method } = request
-    return response.writeHead(200).end("Método: " + method)//uma resposta é obrigatória, se não fica procurando eternamente.
-})
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/products") {
+    return response.end("Lista de produtos!");
+  }
+
+  if (method === "POST" && url === "/products") {
+    return response.end("Produto cadastrado!");
+  }
+
+
+  return response.writeHead(404).end("Rota não encontrada!")
+});
 
 server.listen(3333);
-
